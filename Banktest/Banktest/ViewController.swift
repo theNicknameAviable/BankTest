74cdeee89da6a3206aafe8993d7010aaa9d511a0
Raul Bautista <rbmolina27@gmail.com>
2022-02-01 20:10:22 +0100

    initial commit

---
+++ Banktest/Banktest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Banktest
+//
+//  Created by Raul Bautista on 1/2/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
