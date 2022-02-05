059cd5beebc014e41e25b7527b223943cf277e09
Raul Bautista <rbmolina27@gmail.com>
2022-02-05 12:53:37 +0100

    API Key 2

--- Banktest/Banktest/ViewController.swift
+++ Banktest/Banktest/ViewController.swift
@@ -11,6 +11,7 @@ import Alamofire
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet weak var table: UITableView!
+    static let host: String = "https://gateway.marvel.com"
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +20,12 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     func fetchInfo() {
         
-        let url = "https://"
+        let timeStamp = String(format: "%.f", Date().timeIntervalSince1970)
+        let publicKey = "1de4fe3d3a6a89ba1e61bb34889865f1"
+        let privateKey = "8074772204a5fa9445ca96c81837f2b6d85b546b"
+        let auth = "?ts=\(timeStamp)1&apikey=1234&hash=ffd275c5130566a2916217b101f26150"
+        let url = "\(Self.host)/v1/public/characters\(auth)"
+        
         AF.request(url, method: .get, parameters: nil, headers: nil).responseData { response in
             //aquí llega la respuesta del servidor.
             switch response.result {
