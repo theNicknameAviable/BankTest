001e1ea001ee6eaaceaa5e40f5de2bfd2af76d3a
Raul Bautista <rbmolina27@gmail.com>
2022-02-22 19:44:08 +0100

    prepare second view

---
+++ Banktest/Banktest/ViewControllers/ViewControllerCharacter.swift
@@ -0,0 +1,21 @@
+//
+//  ViewControllerCharacter.swift
+//  Banktest
+//
+//  Created by Raul Bautista on 22/2/22.
+//
+
+import Foundation
+import UIKit
+import Alamofire
+
+
+class ViewControllerCharacter: UIViewController{
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+}
